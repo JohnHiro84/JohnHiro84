554b04f1e97497517048300920f1685692410e51
JohnHiro84 <32828383+JohnHiro84@users.noreply.github.com>
2018-09-06 19:47:45 -0700

    Add files via upload

---
+++ fcc/UserAgentString.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var useragent = require('express-useragent');
+var ip = require("ip");
+
+
+
+//Project - Request Header Parser Microservice
+
+app.use(useragent.express());
+
+//This route returns a json object with the IP address, language and
+//useragent source information
+app.get('/IpLangUserAgentSource', function(req, res){
+
+    console.log("from route /IpLangUserAgentSource");
+    console.log(req.headers["accept-language"]);
+    //console.log('remoteAddress:');
+    //console.log(req.connection.remoteAddress);
+
+
+    console.log ( ip.address() );
+
+    //console.log('figuringoutIp');
+    //console.log(req.ip);
+
+    console.log(req.useragent.source);
+
+    res.json({
+      ipaddress: ip.address(),
+      language: req.headers["accept-language"],
+      software: req.useragent.source
+    });
+});
+
+
+app.listen(3000, () => console.log('listening on port 3000'));
