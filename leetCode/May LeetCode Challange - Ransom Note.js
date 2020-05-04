2187244aeb67700aba480fa14318d1d8edf06b2f
Alexander Yambao <32828383+JohnHiro84@users.noreply.github.com>
2020-05-03 20:51:59 -0700

    Removed console.log

--- leetCode/May LeetCode Challange - Ransom Note.js
+++ leetCode/May LeetCode Challange - Ransom Note.js
@@ -4,7 +4,6 @@ var canConstruct = function(ransomNote, magazine){
   let mag = magazine.split('');
   for(char in ransomNote){
     let char_idx = mag.indexOf(ransomNote[char]);
-    console.log(ransomNote[char]);
     if(char_idx >= 0){
       mag.splice(char_idx, 1);
     } else {
