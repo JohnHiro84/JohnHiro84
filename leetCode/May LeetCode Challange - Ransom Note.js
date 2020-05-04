c07c93cb96146cdb21c543a9e1c8bd52595e1f85
Alexander Yambao <32828383+JohnHiro84@users.noreply.github.com>
2020-05-03 20:51:01 -0700

    Removed tests

--- leetCode/May LeetCode Challange - Ransom Note.js
+++ leetCode/May LeetCode Challange - Ransom Note.js
@@ -1,21 +1,4 @@
-//
-//
-// random ransom note string
-// another string has letters from all the magazines
-//
-// can ransom note be made from the magazines?
-// return true or false
-//
-// each letter in mag can only be used once in your ransom note
-//
-//
-// a   b     false
-// aa  ab    false
-// aa  aab   true
-//
-//
-// string.Remove(2, 1);
-// starting index, num chars
+
 
 var canConstruct = function(ransomNote, magazine){
   let mag = magazine.split('');
@@ -30,6 +13,3 @@ var canConstruct = function(ransomNote, magazine){
   }
   return true;
 }
-canConstruct('a', 'b')
-canConstruct('aa', 'ab');
-canConstruct('aa', 'aab');
